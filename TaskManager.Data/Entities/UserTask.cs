10fde5c777aa42d7f7da0b28589e8b9c5477e55e
Akhil RJ <>
2023-08-12 13:08:50 +0100

    feat: Initialize TaskManager solution with Data layer
    - Set up TaskManager.Data project.
    - Defined UserTask entity with appropriate annotations.
    - Configured TaskManagerDbContext with constraints and configurations.

---
+++ TaskManager.Data/Entities/UserTask.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Data.Entities
+{
+    public class Task
+    {
+        public enum TaskStatus
+        {
+            Pending,
+            InProgress,
+            Completed
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public TaskStatus Status { get; set; } = TaskStatus.Pending;
+
+        public DateTime? DueDate { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public DateTime ModifiedDate { get; set; } = DateTime.Now;
+    }
+}

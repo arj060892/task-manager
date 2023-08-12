76f2851c5bfd43d8e2e566f1b124e1b722a2cc1a
Akhil RJ <>
2023-08-12 13:32:55 +0100

    feat: Implement service layer for UserTask entity - Defined IUserTaskService interface for UserTask operations. - Created UserTaskService as the concrete implementation. - Integrated with the IUserTaskRepository for data operations.

---
+++ TaskManager.Service/Implementations/UserTaskService.cs
@@ -0,0 +1,44 @@
+﻿using TaskManager.Data.Entities;
+using TaskManager.Repository.Interfaces;
+using TaskManager.Service.Interfaces;
+
+namespace TaskManager.Service.Implementations
+{
+    /// <summary>
+    /// Service for performing operations on UserTask.
+    /// </summary>
+    public class UserTaskService : IUserTaskService
+    {
+        private readonly IUserTaskRepository _repository;
+
+        public UserTaskService(IUserTaskRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        public async Task<IEnumerable<UserTask>> GetAllTasksAsync()
+        {
+            return await this._repository.GetAllAsync();
+        }
+
+        public async Task<UserTask> GetTaskByIdAsync(int id)
+        {
+            return await this._repository.GetByIdAsync(id);
+        }
+
+        public async Task AddTaskAsync(UserTask userTask)
+        {
+            await this._repository.AddAsync(userTask);
+        }
+
+        public void UpdateTask(UserTask userTask)
+        {
+            this._repository.Update(userTask);
+        }
+
+        public void DeleteTask(UserTask userTask)
+        {
+            this._repository.Delete(userTask);
+        }
+    }
+}
---
+++ TaskManager.Service/Interfaces/IUserTaskService.cs
@@ -0,0 +1,21 @@
+﻿using TaskManager.Data.Entities;
+
+namespace TaskManager.Service.Interfaces
+{
+    /// <summary>
+    /// Provides an interface for UserTask service operations.
+    /// </summary>
+    public interface IUserTaskService
+    {
+        Task<IEnumerable<UserTask>> GetAllTasksAsync();
+
+        Task<UserTask> GetTaskByIdAsync(int id);
+
+        Task AddTaskAsync(UserTask userTask);
+
+        void UpdateTask(UserTask userTask);
+
+        void DeleteTask(UserTask userTask);
+
+    }
+}

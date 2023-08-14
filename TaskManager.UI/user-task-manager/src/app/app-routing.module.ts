3bbe51122c340ae778cc4601adb990ff14a39008
Akhil RJ <>
2023-08-14 17:49:15 +0100

    Implemented routing for task management features. - Set the default route to load the TaskListComponent. - Configured routes for task creation and optional task editing using task id. - Updated navigation links to reflect new routing setup.

--- TaskManager.UI/user-task-manager/src/app/app-routing.module.ts
+++ TaskManager.UI/user-task-manager/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskCreateComponent } from './task-create/task-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'task/:id', component: TaskCreateComponent },
+  { path: 'task', component: TaskCreateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

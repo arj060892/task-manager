7a8dde2cb91f122e67d8d284a7000a3de37a84a6
Akhil RJ <>
2023-08-14 17:35:59 +0100

    feat(task-list): Retrieve task list from API using NgRx store

--- TaskManager.UI/user-task-manager/src/app/task-list/task-list.component.ts
+++ TaskManager.UI/user-task-manager/src/app/task-list/task-list.component.ts
@@ -4,129 +4,21 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DeleteTaskModalComponent } from '../shared/modals/delete-task-modal/delete-task-modal.component';
 import { Store } from '@ngrx/store';
 import * as TaskActions from '../shared/store/actions/task.actions';
+import { Subscription } from 'rxjs';
+import * as fromTasks from '../shared/store/selectors/task.selectors';
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss'],
 })
 export class TaskListComponent implements OnInit {
-  tasks: UserTaskResponseDTO[] = [
-    {
-      id: 1,
-      title: 'Task 1',
-      description: 'Description 1',
-      status: 'Open',
-      dueDate: new Date().toISOString(),
-      startTime: '09:00:00',
-      endTime: '17:00:00',
-      createdDate: new Date().toISOString(),
-      modifiedDate: new Date().toISOString(),
-    },
-    {
-      id: 2,
-      title: 'Task 2',
-      description: 'Description 2',
-      status: 'Open',
-      dueDate: new Date(Date.now() + 24 * 60 * 60 * 1000).toISOString(),
-      startTime: '10:00:00',
-      endTime: '18:00:00',
-      createdDate: new Date().toISOString(),
-      modifiedDate: new Date().toISOString(),
-    },
-    {
-      id: 3,
-      title: 'Task 3',
-      description: 'Description 3',
-      status: 'Open',
-      dueDate: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000).toISOString(),
-      startTime: '11:00:00',
-      endTime: '19:00:00',
-      createdDate: new Date().toISOString(),
-      modifiedDate: new Date().toISOString(),
-    },
-    {
-      id: 4,
-      title: 'Task 4',
-      description: 'Description 4',
-      status: 'Open',
-      dueDate: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000).toISOString(),
-      startTime: '08:00:00',
-      endTime: '16:00:00',
-      createdDate: new Date().toISOString(),
-      modifiedDate: new Date().toISOString(),
-    },
-    {
-      id: 5,
-      title: 'Task 5',
-      description: 'Description 5',
-      status: 'Open',
-      dueDate: new Date(Date.now() + 4 * 24 * 60 * 60 * 1000).toISOString(),
-      startTime: '12:00:00',
-      endTime: '20:00:00',
-      createdDate: new Date().toISOString(),
-      modifiedDate: new Date().toISOString(),
-    },
-    {
-      id: 6,
-      title: 'Task 6',
-      description: 'Description 6',
-      status: 'Open',
-      dueDate: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000).toISOString(),
-      startTime: '09:30:00',
-      endTime: '17:30:00',
-      createdDate: new Date().toISOString(),
-      modifiedDate: new Date().toISOString(),
-    },
-    {
-      id: 7,
-      title: 'Task 7',
-      description: 'Description 7',
-      status: 'Open',
-      dueDate: new Date(Date.now() + 6 * 24 * 60 * 60 * 1000).toISOString(),
-      startTime: '08:30:00',
-      endTime: '16:30:00',
-      createdDate: new Date().toISOString(),
-      modifiedDate: new Date().toISOString(),
-    },
-    {
-      id: 8,
-      title: 'Task 8',
-      description: 'Description 8',
-      status: 'Open',
-      dueDate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toISOString(),
-      startTime: '10:30:00',
-      endTime: '18:30:00',
-      createdDate: new Date().toISOString(),
-      modifiedDate: new Date().toISOString(),
-    },
-    {
-      id: 9,
-      title: 'Task 9',
-      description: 'Description 9',
-      status: 'Open',
-      dueDate: new Date(Date.now() + 8 * 24 * 60 * 60 * 1000).toISOString(),
-      startTime: '11:30:00',
-      endTime: '19:30:00',
-      createdDate: new Date().toISOString(),
-      modifiedDate: new Date().toISOString(),
-    },
-    {
-      id: 10,
-      title: 'Task 10',
-      description: 'Description 10',
-      status: 'Open',
-      dueDate: new Date(Date.now() + 9 * 24 * 60 * 60 * 1000).toISOString(),
-      startTime: '12:30:00',
-      endTime: '20:30:00',
-      createdDate: new Date().toISOString(),
-      modifiedDate: new Date().toISOString(),
-    },
-  ];
-
   todayTasks: UserTaskResponseDTO[] = [];
   upcomingTasks: UserTaskResponseDTO[] = [];
   pastTasks: UserTaskResponseDTO[] = [];
   expandedTaskId: number | null | undefined = null;
+  private tasksSubscription?: Subscription;
 
   constructor(private modalService: NgbModal, private store: Store) {}
 
@@ -147,23 +39,24 @@ export class TaskListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.todayTasks = this.tasks.filter((t) =>
-      this.isToday(new Date(t.dueDate!))
-    );
-    this.upcomingTasks = this.tasks.filter(
-      (t) => new Date(t.dueDate!) > new Date()
-    );
-    this.pastTasks = this.tasks.filter(
-      (t) => new Date(t.dueDate!) < new Date()
-    );
+    this.store.dispatch(TaskActions.loadTasks());
+    this.tasksSubscription = this.store
+      .select(fromTasks.selectAllTasks)
+      .subscribe((tasks) => {
+        const today = moment();
+        this.todayTasks = tasks.filter((t) =>
+          moment(t.dueDate).isSame(today, 'day')
+        );
+        this.upcomingTasks = tasks.filter((t) =>
+          moment(t.dueDate).isAfter(today)
+        );
+        this.pastTasks = tasks.filter((t) => moment(t.dueDate).isBefore(today));
+      });
   }
 
-  isToday(date: Date) {
-    const today = new Date();
-    return (
-      date.getDate() === today.getDate() &&
-      date.getMonth() === today.getMonth() &&
-      date.getFullYear() === today.getFullYear()
-    );
+  ngOnDestroy(): void {
+    if (this.tasksSubscription) {
+      this.tasksSubscription.unsubscribe();
+    }
   }
 }
